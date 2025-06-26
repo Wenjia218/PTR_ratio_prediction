5b9ab177c7c1b6204508a2dad5fc1a8f185417b0
jg0202 <jannik.geissner@icloud.com>
2025-06-26 13:50:11 +0200

    esm2 200 and 500 embed

--- .ipynb_checkpoints/PTR_prediction-checkpoint.sh
+++ .ipynb_checkpoints/PTR_prediction-checkpoint.sh
@@ -7,4 +7,4 @@
 #SBATCH --output=sh_logs/PTR_prediciton_%j.out  # Output file (%j = job ID)
 
 # Run your Python script
-python PTR_prediciton.py data/Table_EV6.tsv data/Table_EV3.tsv
+python PTR_prediciton.py data/esm2_t12_35M_UR50D_200_embed.tsv data/Table_EV3.tsv
--- ESM-2.sh
+++ ESM-2.sh
@@ -8,4 +8,4 @@
 #SBATCH --output=sh_logs/esm2_%j.out  # Output file (%j = job ID)
 
 # Run your Python script
-python ESM-2.py ../esm2_t12_35M_UR50D 500 
+python ESM-2.py ../esm2_t12_35M_UR50D 200 
